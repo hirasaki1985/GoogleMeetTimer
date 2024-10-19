6c7e309a6026b6ecf3b53ae3f8453617b67547ec
hirasaki <hirasaki1985@gmail.com>
2024-10-19 16:53:37 +0900

    react導入

--- plugin/vite.config.js
+++ plugin/vite.config.js
@@ -1,19 +1,29 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import { resolve, join } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig((opt) => {
   return {
-    root: 'src',
+    server: {
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        port: 5173,
+      },
+    },
+    root: resolve(__dirname, 'src'),
+    publicDir: resolve(__dirname, 'public'),
     build: {
-      outDir: '../dist',
+      outDir: resolve(__dirname, 'dist'),
       rollupOptions: {
         input: {
-          index: resolve(__dirname, 'src/index.ts'),
+          // index: resolve(__dirname, 'src/index.ts'),
+          index: join(__dirname, 'src/index.html'),
         },
         output: {
-          entryFileNames: '[name].js',
+          // entryFileNames: '[name].js',
+          chunkFileNames: 'assets/chunk-[hash].js',
         },
       },
     },
-  }
-})
+  };
+});
